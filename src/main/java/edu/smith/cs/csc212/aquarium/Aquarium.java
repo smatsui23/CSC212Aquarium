b67cb7672b7a3470901bd8e3c892d3be78921a07
sara_univ <sara_univ@131.229.116.236>
2020-02-07 16:38:28 -0500

    Working on the first lab, new fish class

--- src/main/java/edu/smith/cs/csc212/aquarium/Aquarium.java
+++ src/main/java/edu/smith/cs/csc212/aquarium/Aquarium.java
@@ -43,20 +43,28 @@ public class Aquarium extends GFX {
 
 	int fish1X = getWidth() + 100;
 	int fish2X = getWidth() + 300;
+	int fish3X = -100;
+	
+	Fish nemo = new Fish(Color.magenta,250,250);
+	Fish marlin = new Fish(Color.orange, 100, 100);
 
 	@Override
 	public void draw(Graphics2D g) {
 		// Draw the "ocean" background.
 		g.setColor(Color.blue);
 		g.fillRect(0, 0, getWidth(), getHeight());
+		
+		nemo.draw(g);
+		marlin.draw(g);
 
+		DrawFish.smallFacingLeft(g, nemo.color, nemo.x, nemo.y);
 		// Draw the fish!
 		DrawFish.facingLeft(g, Color.yellow, fish1X, 200);
 		// Draw the confused fish!
-		DrawFish.facingRight(g, Color.green, fish2X, 300);
+		DrawFish.facingLeft(g, Color.green, fish2X, 300);
 
 		// What if we wanted this little fish to swim, too?
-		DrawFish.smallFacingLeft(g, Color.red, 200, 100);
+		DrawFish.smallFacingRight(g, Color.red, fish3X, 100);
 
 		// Draw our snail!
 		algorithm.draw(g);
@@ -64,6 +72,11 @@ public class Aquarium extends GFX {
 		// Move the fish!
 		fish1X -= 1;
 		fish2X -= 2;
+		fish3X += 3;
+		if (fish3X > getWidth()+50) {
+			fish3X = -100;
+		}
+				
 	}
 
 	public static void main(String[] args) {
